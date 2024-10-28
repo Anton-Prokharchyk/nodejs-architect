4ec0a36a179c972614c8b50a3a27861d2c329e75
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2024-10-28 16:27:22 +0300

    added linting

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,21 +1,21 @@
-import { Logger } from "tslog";
-import { injectable } from "inversify";
-import "reflect-metadata";
+import { Logger } from 'tslog';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
 
 @injectable()
 export default class LoggerService {
-  logger: Logger<any>;
-  constructor() {
-    this.logger = new Logger();
-  }
+	logger: Logger<any>;
+	constructor() {
+		this.logger = new Logger();
+	}
 
-  logInfo(msg: string) {
-    this.logger.info(msg);
-  }
-  logError(msg: string) {
-    this.logger.error(msg);
-  }
-  logWarning(msg: string) {
-    this.logger.warn(msg);
-  }
+	logInfo(msg: string) {
+		this.logger.info(msg);
+	}
+	logError(msg: string) {
+		this.logger.error(msg);
+	}
+	logWarning(msg: string) {
+		this.logger.warn(msg);
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,26 @@
-import { inject, injectable } from "inversify";
-import "reflect-metadata";
+import { inject, injectable } from 'inversify';
+import 'reflect-metadata';
 
-import { TYPES } from "../types";
-import ILogger from "../logger/logger.interface";
-import IUserService from "./userService.itreface";
+import { TYPES } from '../types';
+import ILogger from '../logger/logger.interface';
+import IUserService from './userService.itreface';
 
 export type User = {
-  id: number;
-  name: string;
-  password: string;
+	id: number;
+	name: string;
+	password: string;
 };
 
 @injectable()
 export default class UserService implements IUserService {
-  constructor(@inject(TYPES.LoggerService) private LoggerService: ILogger) {
-    this.LoggerService.logInfo(`UserService successfully initialized`);
-  }
-  async getUser(id: number): Promise<unknown> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({ id, name: "name", password: "password" });
-      }, 1000);
-    });
-  }
+	constructor(@inject(TYPES.LoggerService) private LoggerService: ILogger) {
+		this.LoggerService.logInfo(`UserService successfully initialized`);
+	}
+	async getUser(id: number): Promise<unknown> {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve({ id, name: 'name', password: 'password' });
+			}, 1000);
+		});
+	}
 }
